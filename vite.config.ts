7cb1008970aefa2dfdb6fae628b46f53cbb51ca4
1Channing <c.cloirec4@gmail.com>
2026-01-23 09:50:37 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import pkg from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,6 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
   define: {
-    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+    __APP_VERSION__: JSON.stringify(pkg.version),
   },
 });
